ed68426e42c57404a80776babec528f5c9c9e1af
MalkingMan <arrayalkhozini@gmail.com>
2024-05-14 15:29:26 +0700

    commit 15

---
+++ Constructor.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Mahasiswa
+{
+    public:
+        int nim;
+        string nama;
+    public:
+    Mahasiswa();
+    {
+        nim = 0;
+        nama = " ";
+    };
+    
+    Mahasiswa(int nim)
+    {
+        nim = iNim;
+    };
+
+    Mahasiswa(string iNama)
+    {
+        nama = iNama
+    };
+
+    Mahasiswa((i))
+};
